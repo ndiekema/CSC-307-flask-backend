7c7621a3371c5aa3d51c2851e8893f055be2b00f
Nathan Diekema <ndiekema@calpoly.edu>
2021-04-20 13:58:15 -0700

    First commit of db-access branch

--- sample_backend.py
+++ sample_backend.py
@@ -13,34 +13,34 @@ def hello_world():
     return 'Hello, World!'
 
 users = { 
-    'users_list' :
-    [
-        { 
-         'id' : 'xyz789',
-         'name' : 'Charlie',
-         'job': 'Janitor',
-        },
-        {
-         'id' : 'abc123', 
-         'name': 'Mac',
-         'job': 'Bouncer',
-        },
-        {
-         'id' : 'ppp222', 
-         'name': 'Mac',
-         'job': 'Professor',
-        }, 
-        {
-         'id' : 'yat999', 
-         'name': 'Dee',
-         'job': 'Aspring actress',
-        },
-        {
-         'id' : 'zap555', 
-         'name': 'Dennis',
-         'job': 'Bartender',
-        }
-    ]
+    'users_list' : []
+    # [
+    #     { 
+    #      'id' : 'xyz789',
+    #      'name' : 'Charlie',
+    #      'job': 'Janitor',
+    #     },
+    #     {
+    #      'id' : 'abc123', 
+    #      'name': 'Mac',
+    #      'job': 'Bouncer',
+    #     },
+    #     {
+    #      'id' : 'ppp222', 
+    #      'name': 'Mac',
+    #      'job': 'Professor',
+    #     }, 
+    #     {
+    #      'id' : 'yat999', 
+    #      'name': 'Dee',
+    #      'job': 'Aspring actress',
+    #     },
+    #     {
+    #      'id' : 'zap555', 
+    #      'name': 'Dennis',
+    #      'job': 'Bartender',
+    #     }
+    # ]
 }
 
 # Testing format: http://localhost:5000/users?name=Mac&job=Bouncer
